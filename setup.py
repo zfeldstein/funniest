16e56d3aa1168701c0bc776057ea5964fde3a28b
zack feldstein <zfeldstein@flexion.us>
2019-05-08 16:09:02 -0500

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      install_requires=['markdown']
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
