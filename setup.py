02d3fbcf39b1a2a3ed19ce492988ff8d757eb802
zack feldstein <zfeldstein@flexion.us>
2019-05-08 16:32:49 -0500

    l

--- setup.py
+++ setup.py
@@ -3,10 +3,21 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
+      long_description='Really, the funniest around.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
       url='http://github.com/storborg/funniest',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
-      install_requires=['markdown']
-      license='MIT',
+      scripts=['bin/funniest-joke'],
       packages=['funniest'],
+      install_requires=[
+          'markdown',
+      ],
+      include_package_data=True,
       zip_safe=False)
